37b45d03e765b6fe0608a2b4af65cccb6102a2fa
Winter Thane <saso.basic.2012@gmail.com>
2023-05-23 21:16:25 +0200

    Add project files.

---
+++ ScrollingBackground/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ScrollingBackground
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D _background;
+        private float _scrollVertical;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 1600;
+            _graphics.PreferredBackBufferHeight = 1200;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _background = Content.Load<Texture2D>("Backgrounds/BackgroundBig");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _scrollVertical += 1.0f;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap);
+            _spriteBatch.Draw(_background, Vector2.Zero, new Rectangle(0, (int)(-_scrollVertical), _background.Width, _background.Height), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ScrollingBackground/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new ScrollingBackground.Game1();
+game.Run();
